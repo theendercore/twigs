285fa8b141030ac66bb73dfa91236c832872133c
theendercore <theendercore@gmail.com>
2024-08-07 16:18:26 +0300

    - ported block tags

--- src/main/java/com/ninni/twigs/data/TwigsDataGenerator.java
+++ src/main/java/com/ninni/twigs/data/TwigsDataGenerator.java
@@ -1,11 +1,20 @@
 package com.ninni.twigs.data;
 
 import com.ninni.twigs.Twigs;
+import com.ninni.twigs.data.tags.TwigsBlockTagProvider;
+import com.ninni.twigs.registry.TwigsBlocks;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.data.DataProvider;
+import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Mod.EventBusSubscriber(modid = Twigs.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class TwigsDataGenerator {
@@ -16,10 +25,21 @@ public class TwigsDataGenerator {
     @SubscribeEvent
     public static void onGatherData(GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
+        PackOutput output = generator.getPackOutput();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+        ExistingFileHelper fileHelper = event.getExistingFileHelper();
         boolean server = event.includeServer();
-        generator.addProvider(server, (DataProvider.Factory<TwigsDatapackBuiltinEntriesProviderOld>) output ->
-            new TwigsDatapackBuiltinEntriesProviderOld(output, event.getLookupProvider())
-        );
+
+        generator.addProvider(server, new TwigsDatapackBuiltinEntriesProviderOld(output, lookupProvider));
+
+
+        // Tags
+        TwigsBlockTagProvider blockTags = generator.addProvider(server, new TwigsBlockTagProvider(output, lookupProvider, fileHelper));
+
+    }
+
+    public static List<Block> getModBlocks() {
+        return TwigsBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get).toList();
     }
 
 }
--- src/main/java/com/ninni/twigs/data/TwigsDataGeneratorFabric.java
+++ src/main/java/com/ninni/twigs/data/TwigsDataGeneratorFabric.java
@@ -7,6 +7,7 @@ import com.ninni.twigs.data.tags.TwigsItemTagProvider;
 import com.ninni.twigs.data.worldgen.TwigsConfiguredFeatureProvider;
 import com.ninni.twigs.data.worldgen.TwigsPlacedFeatureProvider;
 import com.ninni.twigs.data.worldgen.TwigsStructureProvider;
+import com.ninni.twigs.registry.TwigsBlocks;
 import com.ninni.twigs.registry.TwigsConfiguredFeatures;
 import com.ninni.twigs.registry.TwigsPlacedFeatures;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
@@ -17,6 +18,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.RegistryObject;
 
 import java.util.List;
 import static com.ninni.twigs.Twigs.MOD_ID;
@@ -52,7 +54,7 @@ public class TwigsDataGeneratorFabric implements DataGeneratorEntrypoint {
     }
 
     public static List<Block> getModBlocks() {
-        return BuiltInRegistries.BLOCK.stream().filter(block -> getId(block).getNamespace().equals(MOD_ID)).toList();
+        return TwigsBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get).toList();
     }
 
     public static ResourceLocation getId(Block block) {
